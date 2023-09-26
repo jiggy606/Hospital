082c4b910c7669396508fef4fdd457f1d7387c9c
Iberi Daniel <danieliberi99@gmail.com>
2023-09-26 16:41:15 +0100

    created welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+
+            // skip text
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  /* Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => 
+                  )); */
+                },
+                child: Text(
+                  "SKIP",
+                  style: TextStyle(
+                    color: Colors.blue.shade800,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 50,),
+
+            // image
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Image.asset("images/doctors.png"),
+            ),
+
+            SizedBox(height: 50,),
+
+            // appointment
+            Text(
+              "Doctors Appointment",
+              style: TextStyle(
+                color: Colors.blue.shade800,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+                wordSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              "Appoint your doctor",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            SizedBox(height: 60),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Material(
+                  color: Colors.blue.shade800,
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: (){
+                      /* Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => 
+                      )); */
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40), 
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),),
+                  ),
+                ),
+                Material(
+                  color: Colors.blue.shade800,
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {
+                      /* Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => 
+                      )); */
+                    },
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
